abf021b72c6df30bbb5cbbbb457e07a3d487f30e
PRATHIK CHANDRA HEGDE <121326671+prathikh1998@users.noreply.github.com>
2023-06-17 04:56:50 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,97 +1,20 @@
-import pyodbc
-import random
-import time
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Azure SQL Database configuration
-server = 'tcp:prathikhegde.database.windows.net,1433'
-database = 'ASSS2'
-username = 'prathikhegde'
-password = 'Tco7890$'
-driver = '{ODBC Driver 18 for SQL Server}'
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-# Create the connection string
-connection_string = f"DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}"
+@app.route('/results')
+def query_results():
+    query_type = request.args.get('query_type')
 
-# Route for random queries
-@app.route('/random_queries', methods=['GET', 'POST'])
-def random_queries():
-    if request.method == 'POST':
-        num_queries = int(request.form['num_queries'])
-        
-        # Connect to Azure SQL Database
-        conn = pyodbc.connect(connection_string)
-        cursor = conn.cursor()
+    # TODO: Perform the appropriate query based on the selected query_type
+    # and retrieve the results from the database
 
-        # Perform random queries
-        query_results = []
-        start_time = time.time()
-        for _ in range(num_queries):
-            # Generate a random ID for the query
-            random_id = random.randint(1, 1000)
-
-            # Execute the query
-            query = f"SELECT * FROM all_month WHERE id = {random_id}"
-            cursor.execute(query)
-            
-            # Fetch the query result
-            result = cursor.fetchone()
-            query_results.append(result)
-
-        # Calculate the time taken for queries
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-
-        # Close the database connection
-        cursor.close()
-        conn.close()
-
-        # Render the results template
-        return render_template('results.html', query_type='Random Queries', results=query_results, elapsed_time=elapsed_time)
-
-    return render_template('random_queries.html')
-
-# Route for restricted queries
-@app.route('/restricted_queries', methods=['GET', 'POST'])
-def restricted_queries():
-    if request.method == 'POST':
-        # Retrieve the user's selection criteria
-        latitude = float(request.form['latitude'])
-        longitude = float(request.form['longitude'])
-        distance = int(request.form['distance'])
-
-        # Connect to Azure SQL Database
-        conn = pyodbc.connect(connection_string)
-        cursor = conn.cursor()
-
-        # Perform restricted queries
-        query_results = []
-        start_time = time.time()
-
-        # Execute the query
-        query = f"SELECT * FROM all_month WHERE geography::Point({latitude}, {longitude}, 4326).STDistance(geography::Point(latitude, longitude, 4326)) < {distance}"
-        cursor.execute(query)
-
-        # Fetch all query results
-        results = cursor.fetchall()
-        query_results.extend(results)
-
-        # Calculate the time taken for the query
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-
-        # Close the database connection
-        cursor.close()
-        conn.close()
-
-        # Render the results template
-        return render_template('results.html', query_type='Restricted Queries', results=query_results, elapsed_time=elapsed_time)
-
-    return render_template('restricted_queries.html')
-
-# Rest of your application code...
+    # Pass the query results to the results.html template for rendering
+    return render_template('results.html', query_type=query_type, results=results)
 
 if __name__ == '__main__':
     app.run()
