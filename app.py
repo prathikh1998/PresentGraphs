c88188a6fdfd3c8f4452b90561a22ee42ad19983
PRATHIK CHANDRA HEGDE <121326671+prathikh1998@users.noreply.github.com>
2023-06-17 05:14:39 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -3,18 +3,18 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-@app.route('/results')
-def query_results():
-    query_type = request.args.get('query_type')
-
-    # TODO: Perform the appropriate query based on the selected query_type
-    # and retrieve the results from the database
-
-    # Pass the query results to the results.html template for rendering
-    return render_template('results.html', query_type=query_type, results=results)
+@app.route('/results.html', methods=['POST'])
+def results():
+    # Retrieve the query parameters from the form submission
+    query_type = request.form.get('query-type')
+    # Process the query parameters and execute the appropriate SQL queries
+    # Retrieve the query results
+    query_results = ['Result 1', 'Result 2', 'Result 3']  # Replace with actual query results
+    # Pass the results to the results.html template for rendering
+    return render_template('results.html', query_results=query_results)
 
 if __name__ == '__main__':
     app.run()
