7388372fc20c332f74bfce6fba7e0d7559a82efe
PRATHIK CHANDRA HEGDE <121326671+prathikh1998@users.noreply.github.com>
2023-06-20 02:42:19 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -13,8 +13,7 @@ cnxn = pyodbc.connect(connection_string)
 cursor = cnxn.cursor()
 
 # Connect to your Azure Redis cache
-redis_connection_string = "quizredis.redis.cache.windows.net:6380,password=nkp3itVINJCqSXZDygXgqoo1baX48GwDTAzCaM4ZFX0=,ssl=True,abortConnect=False"
-redis_cache = redis.from_url(redis_connection_string)
+redis_connection = redis.Redis.from_url("redis://quizredis.redis.cache.windows.net:6380?password=nkp3itVINJCqSXZDygXgqoo1baX48GwDTAzCaM4ZFX0=&ssl=True&abortConnect=False")
 
 @app.route('/')
 def index():
@@ -27,20 +26,18 @@ def random_queries():
 
         query_results = []
         for _ in range(num_queries):
-            start_time = time.time()  # Start time for each query
-
             # Generate a random query
             query = generate_random_query()
 
-            # Check if the result exists in the cache
-            result = redis_cache.get(query)
+            # Check if the query result is already in Redis cache
+            result = redis_connection.get(query)
 
             if result:
-                # Result found in the cache
-                query_time = "From Cache"
-                result = eval(result)  # Convert the cached result from string to tuple
+                # If the result is in cache, use it directly
+                query_results.append((query, float(result)))
             else:
                 # Execute the query
+                start_time = time.time()
                 cursor.execute(query)
 
                 # Fetch the results (optional)
@@ -49,10 +46,10 @@ def random_queries():
                 # Get the execution time
                 query_time = time.time() - start_time
 
-                # Store the result in the cache
-                redis_cache.set(query, str((results, query_time)))  # Store the result as a string
+                # Store the query result in Redis cache
+                redis_connection.set(query, query_time)
 
-            query_results.append((query, result))
+                query_results.append((query, query_time))
 
         return render_template('results.html', query_results=query_results)
     else:
@@ -66,20 +63,18 @@ def restricted_queries():
 
         query_results = []
         for _ in range(num_queries):
-            start_time = time.time()  # Start time for each query
-
             # Generate a random restricted query
             query = generate_random_restricted_query()
 
-            # Check if the result exists in the cache
-            result = redis_cache.get(query)
+            # Check if the query result is already in Redis cache
+            result = redis_connection.get(query)
 
             if result:
-                # Result found in the cache
-                query_time = "From Cache"
-                result = eval(result)  # Convert the cached result from string to tuple
+                # If the result is in cache, use it directly
+                query_results.append((query, float(result)))
             else:
                 # Execute the query
+                start_time = time.time()
                 cursor.execute(query)
 
                 # Fetch the results (optional)
@@ -88,69 +83,17 @@ def restricted_queries():
                 # Get the execution time
                 query_time = time.time() - start_time
 
-                # Store the result in the cache
-                redis_cache.set(query, str((results, query_time)))  # Store the result as a string
+                # Store the query result in Redis cache
+                redis_connection.set(query, query_time)
 
-            query_results.append((query, result))
+                query_results.append((query, query_time))
 
         return render_template('results.html', query_results=query_results)
     else:
         return render_template('restricted_queries.html')
 
 
-def generate_random_query():
-    table_name = "all_month"
-    fields = [
-        "time", "latitude", "longitude", "depth", "mag", "magType", "nst", "gap", "dmin", "rms",
-        "net", "id", "updated", "place", "type", "horizontalError", "depthError", "magError",
-        "magNst", "status", "locationSource", "magSource"
-    ]
-
-    # Generate a random query to fetch a random tuple
-    random_field = random.choice(fields)
-    query = f"SELECT TOP 1 {random_field} FROM {table_name} ORDER BY NEWID();"
-
-    return query
-
-
-def generate_random_restricted_query():
-    table_name = "all_month"
-    fields = [
-        "time", "latitude", "longitude", "depth", "mag", "magType", "nst", "gap", "dmin", "rms",
-        "net", "id", "updated", "place", "type", "horizontalError", "depthError", "magError",
-        "magNst", "status", "locationSource", "magSource"
-    ]
-
-    # Generate a random restricted condition
-    condition = generate_random_restricted_condition()
-
-    # Generate a random query with the condition
-    random_field = random.choice(fields)
-    query = f"SELECT TOP 1 {random_field} FROM {table_name} WHERE {condition} ORDER BY NEWID();"
-
-    return query
-
-
-def generate_random_restricted_condition():
-    conditions = [
-        "place LIKE '%CA%'",
-        f"time BETWEEN '{generate_random_date()}' AND '{generate_random_date()}'",
-        f"mag BETWEEN {random.uniform(0, 10)} AND {random.uniform(0, 10)}"
-    ]
-
-    # Generate a random restricted condition
-    condition = random.choice(conditions)
-
-    return condition
-
-
-def generate_random_date():
-    # Generate a random date string between 2000-01-01 and 2023-12-31
-    start_date = datetime.datetime(2000, 1, 1)
-    end_date = datetime.datetime(2023, 12, 31)
-    random_date = start_date + (end_date - start_date) * random.random()
-    return random_date.strftime('%Y-%m-%d')
-
+# Rest of the code...
 
 if __name__ == '__main__':
     app.run()
