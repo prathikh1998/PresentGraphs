58bba012a01c36ee94809248c5ac3eab214a0c9d
PRATHIK CHANDRA HEGDE <121326671+prathikh1998@users.noreply.github.com>
2023-06-21 05:07:58 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ import datetime
 import time
 import pyodbc
 import redis
+import json
 
 app = Flask(__name__)
-cache = redis.Redis(host='quizredis.redis.cache.windows.net', port=6379, password='ynkp3itVINJCqSXZDygXgqoo1baX48GwDTAzCaM4ZFX0=', ssl=False)
+cache = redis.Redis(host='quizredis.redis.cache.windows.net', port=6380, password='ynkp3itVINJCqSXZDygXgqoo1baX48GwDTAzCaM4ZFX0=', ssl=True)
 
 # Connect to your Azure SQL database
 connection_string = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=tcp:prathikhegde.database.windows.net,1433;DATABASE=ASSS2;UID=prathikhegde;PWD=Tco7890$"
@@ -18,13 +19,9 @@ def index():
     return render_template('index.html')
 
 
-
-# ...
-
 @app.route('/random_queries', methods=['POST', 'GET'])
 def random_queries():
     try:
-        
         if request.method == 'POST':
             num_queries = int(request.form.get('num_queries'))
 
@@ -32,21 +29,21 @@ def random_queries():
             total_time = 0  # Initialize total time
 
             for _ in range(num_queries):
-            # Generate a random query
+                # Generate a random query
                 query = generate_random_query()
 
-            # Check if the result is cached
+                # Check if the result is cached
                 start_time = time.time()  # Start the timer
                 result = fetch_results_from_cache(query)
                 query_time = time.time() - start_time
 
                 if result is None:
-                # Execute the query and fetch the results
+                    # Execute the query and fetch the results
                     start_time = time.time()  # Start the timer
                     cursor.execute(query)
                     results = cursor.fetchall()
 
-                # Convert the pyodbc.Row objects to dictionaries
+                    # Convert the pyodbc.Row objects to dictionaries
                     rows = []
                     for row in results:
                         row_dict = {}
@@ -54,17 +51,17 @@ def random_queries():
                             row_dict[column[0]] = row[idx]
                         rows.append(row_dict)
 
-                # Get the execution time
+                    # Get the execution time
                     query_time += time.time() - start_time
                     total_time += query_time  # Add query time to the total
 
                     query_results.append((query, query_time, rows))
 
-                # Cache the results
-                    cache_results(query, str(rows))
+                    # Cache the results
+                    cache_results(query, rows)
                 else:
-                # Use the cached results
-                    query_results.append((query, query_time, eval(result)))
+                    # Use the cached results
+                    query_results.append((query, query_time, result))
 
             return render_template('results.html', query_results=query_results, total_time=total_time)
         else:
@@ -111,30 +108,30 @@ def restricted_queries():
                 query_results.append((query, query_time, rows))
 
                 # Cache the results
-                cache_results(query, str(rows))
+                cache_results(query, rows)
             else:
                 # Use the cached results
-                query_results.append((query, query_time, eval(result)))
+                query_results.append((query, query_time, result))
 
         return render_template('results.html', query_results=query_results, total_time=total_time)
     else:
         return render_template('restricted_queries.html')
 
-# ...
 
 def fetch_results_from_cache(query):
     # Check if the query result is cached
-    if cache.exists(query):
-        # Fetch the cached result
-        result = cache.get(query)
-        return result.decode('utf-8')
+    result = cache.get(query)
+
+    if result is not None:
+        # Decode the cached result and return it
+        return json.loads(result)
 
     return None
 
 def cache_results(query, result):
-    # Cache the query result
-    cache.set(query, result)
-    
+    # Convert the result to a JSON string before caching
+    cache.set(query, json.dumps(result))
+
 def generate_random_query():
     table_name = "all_month"
     fields = [
@@ -183,8 +180,8 @@ def generate_random_restricted_condition():
 
 def generate_random_date():
     # Generate a random date string between 2000-01-01 and 2023-12-31
-    start_date = datetime.datetime(2023, 8, 1)
-    end_date = datetime.datetime(2023, 10, 31)
+    start_date = datetime.datetime(2000, 1, 1)
+    end_date = datetime.datetime(2023, 12, 31)
     random_date = start_date + (end_date - start_date) * random.random()
     return random_date.strftime('%Y-%m-%d')
 
