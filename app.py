5691e5fdada13d870e64b95a7a382cb3bdc42b06
PRATHIK CHANDRA HEGDE <121326671+prathikh1998@users.noreply.github.com>
2023-06-22 03:17:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,51 +14,30 @@ driver = '{ODBC Driver 17 for SQL Server}'
 # Route for displaying the chart in HTML
 @app.route('/')
 def display_chart():
-    # Connect to Azure SQL Database
-    connection_string = f"DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}"
+    # Connect to the database
     conn = pyodbc.connect(connection_string)
-
-    # Execute the query
-    query = """
-        SELECT
-            magnitude_range,
-            COUNT(*) AS quake_count
-        FROM (
-            SELECT
-                CASE
-                    WHEN mag < 1 THEN 'Magnitude < 1'
-                    WHEN mag >= 1 AND mag < 2 THEN 'Magnitude 1-2'
-                    WHEN mag >= 2 AND mag < 3 THEN 'Magnitude 2-3'
-                    WHEN mag >= 3 AND mag < 4 THEN 'Magnitude 3-4'
-                    WHEN mag >= 4 AND mag <= 5 THEN 'Magnitude 4-5'
-                    ELSE 'Magnitude > 5'
-                END AS magnitude_range
-            FROM
-                all_month
-        ) AS subquery
-        GROUP BY
-            magnitude_range
-        ORDER BY
-            CASE magnitude_range
-                WHEN 'Magnitude < 1' THEN 1
-                WHEN 'Magnitude 1-2' THEN 2
-                WHEN 'Magnitude 2-3' THEN 3
-                WHEN 'Magnitude 3-4' THEN 4
-                WHEN 'Magnitude 4-5' THEN 5
-                ELSE 6
-            END
-    """
-
     cursor = conn.cursor()
-    cursor.execute(query)
 
-    # Fetch the query results
-    results = cursor.fetchall()
+    # Execute the SQL query
+    cursor.execute("SELECT magnitude_range, quake_count FROM (SELECT CASE WHEN mag < 1 THEN 'Magnitude < 1' WHEN mag >= 1 AND mag < 2 THEN 'Magnitude 1-2' WHEN mag >= 2 AND mag < 3 THEN 'Magnitude 2-3' WHEN mag >= 3 AND mag < 4 THEN 'Magnitude 3-4' WHEN mag >= 4 AND mag <= 5 THEN 'Magnitude 4-5' ELSE 'Magnitude > 5' END AS magnitude_range, COUNT(*) AS quake_count FROM all_month GROUP BY mag) AS subquery ORDER BY magnitude_range")
 
-    # Convert the results to JSON format
+    # Fetch all the rows
+    rows = cursor.fetchall()
+
+    # Convert rows to a list of dictionaries
+    results = []
+    for row in rows:
+        result = {'magnitude_range': row[0], 'quake_count': row[1]}
+        results.append(result)
+
+    # Close the database connection
+    cursor.close()
+    conn.close()
+
+    # Convert the results to JSON serializable format
     data = json.dumps(results)
 
-    # Render the HTML template with the chart data
+    # Render the template with the data
     return render_template('index.html', data=data)
 
 if __name__ == '__main__':
