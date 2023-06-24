34173ba419050fb441b9ddf250e544bd6b2c54ff
PRATHIK CHANDRA HEGDE <121326671+prathikh1998@users.noreply.github.com>
2023-06-24 05:18:29 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def chart_config():
 # Route for generating the chart based on user input
 # Route for generating the chart based on user input
 @app.route('/generate_chart_population', methods=['POST'])
-def generate_chart():
+def generate_chart_population():
     attribute = request.form.get('attribute')
     intervals = request.form.getlist('interval')
 
@@ -77,6 +77,56 @@ def generate_chart():
     # Return the chart data as JSON
     return render_template('index.html', data=data)
 
+#city function
+
+@app.route('/generate_chart_city', methods=['POST'])
+def generate_chart_city():
+    attribute = request.form.get('attribute')
+    intervals = request.form.getlist('interval')
+
+    conditions = []
+
+    for interval in intervals:
+        condition = f"{attribute} = '{interval}'"
+        conditions.append(condition)
+
+    query_statement = " OR ".join(conditions)
+
+    sql_query = f"""
+        SELECT {attribute}, COUNT(*) AS count
+        FROM [city-1]
+        WHERE {query_statement}
+        GROUP BY {attribute}
+        ORDER BY {attribute}
+    """
+
+    print("Generated SQL query:")
+    print(sql_query)
+
+    # Connect to the database
+    conn = pyodbc.connect(connection_string)
+    cursor = conn.cursor()
+
+    # Execute the SQL query
+    cursor.execute(sql_query)
+
+    # Fetch all the rows
+    rows = cursor.fetchall()
+
+    # Convert rows to a list of dictionaries
+    results = []
+    for row in rows:
+        result = {'attribute_value': row[0], 'count': row[1]}
+        results.append(result)
+
+    # Close the database connection
+    cursor.close()
+    conn.close()
+
+    data = json.dumps(results)
+
+    # Return the chart data as JSON
+    return render_template('index.html', data=data)
 
 
 if __name__ == '__main__':
