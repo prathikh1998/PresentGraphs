02b4841756a3bbe6b693282dd6a8b6124f3d6d3e
PRATHIK CHANDRA HEGDE <121326671+prathikh1998@users.noreply.github.com>
2023-06-24 03:18:36 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def generate_chart():
             conditions.append(condition)
 
     query_statement = " ".join([f"WHEN {condition} THEN '{interval}'" for condition, interval in zip(conditions, intervals) if interval != 'gt'])
-    case_statement = " ".join([f"WHEN {attribute}_range = '{interval}' THEN '{interval}'" for interval, interval in zip(intervals,intervals) if interval != 'gt']) 
+    case_statement = " ".join([f"WHEN {attribute}_range '{interval}' THEN '{interval}'" for interval, interval in zip(intervals,intervals) if interval != 'gt']) 
     sql_query = f"""
         SELECT {attribute}_range, COUNT(*) AS count
         FROM (
@@ -44,7 +44,7 @@ def generate_chart():
             FROM [city-1]
         ) AS subquery
         GROUP BY {attribute}_range
-        ORDER BY CASE {case_statement} {gt_condition} ELSE 'Other' END
+        ORDER BY CASE {attribute}_range {case_statement} {gt_condition} ELSE 'Other' END
     """
 
     print("Generated SQL query:")
