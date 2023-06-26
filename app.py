544464bb7f514c8cfab51f26ac8b29d39477cc35
PRATHIK CHANDRA HEGDE <121326671+prathikh1998@users.noreply.github.com>
2023-06-26 05:33:25 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -75,6 +75,68 @@ def generate_chart_population():
     # Return the chart data as JSON
     return render_template('index.html', data=data)
 
+#magnitude 
+
+@app.route('/generate_chart_mag', methods=['POST'])
+def generate_chart_population():
+    attribute = request.form.get('attribute')
+    intervals = request.form.getlist('interval')
+
+    conditions = []
+    gt_condition = ""
+
+    for interval in intervals:
+        if interval == 'gt':
+            gt_condition = f"WHEN {attribute} > '{max_val}' THEN '>{max_val}'"
+        else:
+            min_val, max_val = interval.split('-')
+            condition = f"{attribute} >= '{min_val}' AND {attribute} < '{max_val}'"
+            conditions.append(condition)
+
+    query_statement = " ".join([f"WHEN {condition} THEN '{interval}'" for condition, interval in zip(conditions, intervals) if interval != 'gt'])
+    case_statement = " ".join([f"WHEN '{interval}' THEN '{interval}'" for interval, interval in zip(intervals, intervals) if interval != 'gt']) 
+    sql_query = f"""
+        SELECT {attribute}_range, COUNT(*) AS count
+        FROM (
+            SELECT CASE {query_statement} {gt_condition} ELSE 'Other' END AS {attribute}_range
+            FROM all_month
+        ) AS subquery
+        GROUP BY {attribute}_range
+        ORDER BY CASE {attribute}_range {case_statement} {gt_condition} ELSE 'Other' END
+    """
+
+    print("Generated SQL query:")
+    print(sql_query)
+
+    # Connect to the database
+    conn = pyodbc.connect(connection_string)
+    cursor = conn.cursor()
+
+    # Execute the SQL query
+    cursor.execute(sql_query)
+
+    # Fetch all the rows
+    rows = cursor.fetchall()
+
+    # Convert rows to a list of dictionaries
+    results = []
+    for row in rows:
+        result = {'attribute_value': row[0], 'count': row[1]}
+        results.append(result)
+
+    # Close the database connection
+    cursor.close()
+    conn.close()
+
+    data = json.dumps(results)
+
+    # Return the chart data as JSON
+    return render_template('index.html', data=data)
+
+
+
+
+
 # Route for generating the chart based on city attribute
 @app.route('/generate_chart_city', methods=['POST'])
 def generate_chart_city():
