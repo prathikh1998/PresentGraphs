fbeae71e9206c93f22f198b00598bca4f97aa0b9
PRATHIK CHANDRA HEGDE <121326671+prathikh1998@users.noreply.github.com>
2023-06-27 00:01:54 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,4 @@
 from flask import Flask, render_template, request, jsonify
-import pyodbc
-import json
-from decimal import Decimal
-
-app = Flask(__name__)
-
-# Azure SQL Database connection settings
-server = 'prathikhegde.database.windows.net'
-database = 'ASSS2'
-username = 'prathikhegde'
-password = 'Tco7890$'
-driver = '{ODBC Driver 17 for SQL Server}'
-connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
-
-# Route for displaying the chart configuration form
-from flask import Flask, render_template, request, jsonify
 from decimal import Decimal
 
 app = Flask(__name__)
@@ -23,13 +7,14 @@ app = Flask(__name__)
 def chart_config():
     return render_template('chart.html')
 
-
 @app.route('/process_tuples', methods=['POST'])
 def process_tuples():
-    tuples = request.form.get('json_data')
-    tuples = json.loads(tuples)
+    tuples = []
+    letters = request.form.getlist('letters[]')
+    amounts = request.form.getlist('amounts[]')
 
-    print(tuples)
+    for letter, amount in zip(letters, amounts):
+        tuples.append({'letter': letter, 'amount': amount})
 
     # Perform further processing with the tuples
 
@@ -42,11 +27,8 @@ def process_tuples():
         for tuple in tuples
     ]
 
-    print(data)
-
     # Render the index.html template with the data
     return render_template('index.html', data=data)
-
 
 if __name__ == '__main__':
     app.run()
